b6cc069ceb7c19f29f4d40dd5a409cb0fce5a66f
scosijn <17729237+scosijn@users.noreply.github.com>
2021-12-31 21:29:57 +0100

    rename playback

---
+++ code/recording.py
@@ -0,0 +1,35 @@
+import retro
+import os
+import glob
+import re
+import time
+
+
+def play_recording(path):
+    movie = retro.Movie(path)
+    movie.step()
+    env = retro.make(
+        game=movie.get_game(),
+        state=None,
+        use_restricted_actions=retro.Actions.ALL,
+        players=movie.players
+    )
+    env.initial_state = movie.get_state()
+    env.reset()
+    while movie.step():
+        actions = []
+        for p in range(movie.players):
+            for i in range(env.num_buttons):
+                actions.append(movie.get_key(i, p))
+        env.step(actions)
+        env.render()
+        time.sleep(0.001)
+    env.close()
+    movie.close()
+
+
+def play_all_recordings(path):
+    files = glob.glob(os.path.join(path, '*.bk2'))
+    for file_path in sorted(files, key=lambda x:int(re.findall('(\d+)',x)[0])):
+        print(f'playing {file_path}')
+        play_recording()(file_path)
