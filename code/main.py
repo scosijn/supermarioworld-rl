edc30f64b79c9d6df4c1db704a19cac57226001f
scosijn <17729237+scosijn@users.noreply.github.com>
2022-01-01 19:08:23 +0100

    remove y_pos reward

--- code/main.py
+++ code/main.py
@@ -15,11 +15,11 @@ def create_env(state):
     )
     env = wrap_env(env)
     check_env(env)
+    print_env_info(env)
     return env
 
 
 def print_env_info(env):
-    print('system: {}'.format(env.system))
     print('gamename: {}'.format(env.gamename))
     print('statename: {}'.format(env.statename))
     print('buttons: {}'.format(env.buttons))
@@ -28,28 +28,7 @@ def print_env_info(env):
     print('reward_range: {}'.format(env.reward_range))
 
 
-def test_random_agent(env):
-    env.reset()
-    while True:
-        _, _, done, _ = env.step(env.action_space.sample())
-        env.render()
-        if done:
-            env.reset()
-
-
-def train_model(model, total_timesteps, save_freq, name_prefix='model', verbose=1):
-    checkpoint_callback = SaveCheckpoint(
-        save_freq=save_freq,
-        name_prefix=name_prefix,
-        model_path='./models/',
-        record_path='./playback/',
-        verbose=verbose
-    )
-    with ProgressBar(total_timesteps) as progress_callback:
-        model.learn(total_timesteps, callback=[checkpoint_callback, progress_callback])
-
-
-def create_PPO_model(env):
+def PPO_model(env):
     model = PPO(policy='CnnPolicy',
                 env=env,
                 learning_rate=lambda f : f * 2.5e-4,
@@ -64,23 +43,38 @@ def create_PPO_model(env):
     return model
 
 
-def test_PPO(env, model_name):
-    model = PPO.load('./models/{}'.format(model_name))
+def train_model(model, total_timesteps, save_freq, name_prefix='model', verbose=1):
+    checkpoint_callback = SaveCheckpoint(
+        save_freq=save_freq,
+        name_prefix=name_prefix,
+        model_path='./models/',
+        record_path='./playback/',
+        verbose=verbose
+    )
+    with ProgressBar(total_timesteps) as progress_callback:
+        model.learn(total_timesteps, callback=[checkpoint_callback, progress_callback])
+
+
+def test_model(env, model_name):
+    model = PPO.load(f'./models/{model_name}')
     obs = env.reset()
-    while True:
+    done = False
+    while not done:
         action, _ = model.predict(obs)
         obs, _, done, _ = env.step(action)
         env.render()
         if done:
             obs = env.reset()
+    env.close()
 
 
 def main():
     env = create_env('YoshiIsland2')
-    model = create_PPO_model(env)
-    train_model(model, 20000, 10000, 'PPO_Mario')
-    print('done')
-    return
+    model = PPO_model(env)
+    train_model(model,
+                total_timesteps=1_000_000,
+                save_freq=100_000,
+                name_prefix='mario_ppo')
 
 
 if __name__ == '__main__':
