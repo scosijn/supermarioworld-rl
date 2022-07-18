43d94bfa63594083eb01ce3e80028bf61b7f583a
scosijn <17729237+scosijn@users.noreply.github.com>
2022-07-18 15:31:27 +0200

    grid search

--- code/main.py
+++ code/main.py
@@ -28,17 +28,30 @@ def make_retro_env(state, verbose=0):
     return env
 
 
+#def PPO_model(env, log='./tensorboard/'):
+#    model = PPO(policy='CnnPolicy',
+#                env=env,
+#                learning_rate=lambda f: f * 2.5e-4,
+#                n_steps=128,
+#                batch_size=4,
+#                n_epochs=4,
+#                ent_coef=.01,
+#                clip_range=0.1,
+#                gamma=0.99,
+#                gae_lambda=0.95,
+#                tensorboard_log=log)
+#    return model
+
+
 def PPO_model(env, log='./tensorboard/'):
     model = PPO(policy='CnnPolicy',
                 env=env,
-                learning_rate=lambda f : f * 2.5e-4,
+                learning_rate=lambda f: f * 2.5e-4,
                 n_steps=128,
-                batch_size=4,
                 n_epochs=4,
-                ent_coef=.01,
-                clip_range=0.1,
-                gamma=0.99,
-                gae_lambda=0.95,
+                batch_size=32,
+                clip_range=lambda f: f * 0.1,
+                ent_coef=0.01,
                 tensorboard_log=log)
     return model
 
@@ -68,6 +81,7 @@ def train_model(model, total_timesteps, save_freq, name_prefix='model', verbose=
         model.learn(
             total_timesteps,
             reset_num_timesteps=False,
+            tb_log_name=name_prefix,
             callback=[checkpoint_callback, progress_callback]
         )
 
@@ -103,13 +117,35 @@ def random_agent(env, infinite=False):
     env.close()
 
 
+def grid_search():
+    n_steps_arr = [(128, 'steps128')]
+    batch_size_arr = [(32, 'batch32'), (128, 'batch128')]
+    clip_range_arr = [(0.2, 'const02')] #[(lambda f: f * 0.1, 'lin01'), (0.1, 'const01'), (0.2, 'const02')]
+    for steps_value, steps_name in n_steps_arr:
+        for batch_value, batch_name in batch_size_arr:
+            for clip_value, clip_name in clip_range_arr:
+                model_name = steps_name + '_' + batch_name + '_' + clip_name
+                env = make_retro_env('YoshiIsland2')
+                model = PPO(policy='CnnPolicy',
+                            env=env,
+                            learning_rate=lambda f: f * 2.5e-4,
+                            n_steps=steps_value,
+                            batch_size=batch_value,
+                            n_epochs=4,
+                            clip_range=clip_value,
+                            ent_coef=0.01,
+                            seed=123,
+                            tensorboard_log='./tensorboard/')
+                train_model(model,
+                            total_timesteps=500_000,
+                            save_freq=0,
+                            name_prefix=model_name)
+                model.save(model_name)
+                env.close()
+
+
 def main():
     env = make_retro_env('YoshiIsland2')
-    model = PPO_model(env)
-    train_model(model,
-                total_timesteps=1_000_000,
-                save_freq=100_000,
-                name_prefix='mario_ppo')
 
 
 if __name__ == '__main__':
